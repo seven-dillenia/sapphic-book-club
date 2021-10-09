90dd98c3ada1dba5d25b253c36e4690120048c4e
Seven Dillenia <sevendillenia@gmail.com>
2021-10-09 23:41:29 +1100

    add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { NoticeDialog } from './modules/home/partial/notice-dialog/noticeDialog.component';
-import { Helper } from './shared/utils/helper';
-import { TypeUtil } from './shared/utils/type-utils';
-import { LocalStorageModel } from './models/localStorageModel';
-
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { NoticeDialog } from "./modules/home/partial/notice-dialog/noticeDialog.component";
+import { Helper } from "./shared/utils/helper";
+import { TypeUtil } from "./shared/utils/type-utils";
+import { LocalStorageModel } from "./models/localStorageModel";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs/operators";
+declare let gtag;
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
     <div class="sapphic container">
       <div class="sapphic__row container__row">
@@ -15,35 +17,40 @@ import { LocalStorageModel } from './models/localStorageModel';
       <!-- <div class="sapphic__row container__row">
         <app-filter></app-filter>
       </div> -->
-      <main>
+      <!-- <main>
         <router-outlet></router-outlet>
-      </main>
+      </main> -->
     </div>
     <footer>
       <sbc-footer></sbc-footer>
     </footer>
   `,
-  styles: []
+  styles: [],
 })
-
-export class AppComponent implements OnInit {
-  constructor(public dialog: MatDialog) { }
-
-  ngOnInit() {
+export class AppComponent {
+  constructor(public dialog: MatDialog, private router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter((evn) => evn instanceof NavigationEnd)
+    );
+    navEndEvents.subscribe((env: NavigationEnd) => {
+      gtag("config", "G-C2RMTTQ2ZG", {
+        'page_path': env.urlAfterRedirects
+      });
+    })
     this.openNoticeDialog();
   }
 
   openNoticeDialog(): void {
     const localStorageKey: keyof LocalStorageModel = "hasShownNotice1";
     const localStorageValue = localStorage.getItem(localStorageKey);
-    const hasShownNotice1 = TypeUtil.stringToBoolean(localStorageValue || '');
+    const hasShownNotice1 = TypeUtil.stringToBoolean(localStorageValue || "");
 
     if (!hasShownNotice1) {
       this.dialog.open(NoticeDialog, {
-        maxWidth: '500px',
+        maxWidth: "500px",
       });
 
-      localStorage.setItem(localStorageKey, 'true');
+      localStorage.setItem(localStorageKey, "true");
     }
   }
 }
